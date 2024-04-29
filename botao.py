6720e28b80d02483b61fcba5a0eff8e40c4fdc62
ZunHigino <anavcavalcantef@gmail.com>
2024-04-29 15:17:23 -0300

    slider

--- botao.py
+++ botao.py
@@ -1,15 +1,21 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-
-def botao_pressionado(instance):
-    print('Botão foi pressionado!')
+from kivy.uix.slider import Slider
 
 class MeuApp(App):
     def build(self):
-        btn = Button(text='Clique em mim!')
-        btn.bind(on_press=botao_pressionado)
-        return btn
+        layout = BoxLayout(orientation='vertical',padding=20)
+        slider= Slider(min=0, max=100, value=50, step=0.5)
+        slider.bind(value=self.atualizar_label)
+        self.label = Label(text = f'Volume:{(slider.value)}')
+        
+        layout.add_widget(slider)
+        layout.add_widget(self.label)
+        
+        return layout
     
+    def atualizar_label(self, instance, valor):
+        self.label.text = f'Volume: {(valor)}'
+        
 MeuApp().run()
